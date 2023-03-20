6d9712f2a0817a2bbd9501de420c5bbfabcb1e61
u-ryu-00 <lwh76104@gmail.com>
2023-03-20 12:23:15 +0900

    수정

--- application/ReadingApplication/src/main/java/FavoritesPanel.java
+++ application/ReadingApplication/src/main/java/FavoritesPanel.java
@@ -3,5 +3,6 @@ import javax.swing.*;
 public class FavoritesPanel extends JPanel {
     FavoritesPanel() {
         this.add(new JLabel("즐겨찾기 패널"));
+        this.add(new JLabel("즐겨찾기 패널입니다."));
     }
 }

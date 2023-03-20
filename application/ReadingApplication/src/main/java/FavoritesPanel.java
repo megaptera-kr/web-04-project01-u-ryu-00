860f2063eb45de2a31d3d8a2a27bd97dbb20ad15
u-ryu-00 <lwh76104@gmail.com>
2023-03-20 11:13:20 +0900

    프로젝트 레이아웃

---
+++ application/ReadingApplication/src/main/java/FavoritesPanel.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+public class FavoritesPanel extends JPanel {
+    FavoritesPanel() {
+        this.add(new JLabel("즐겨찾기 패널"));
+    }
+}
---
+++ application/ReadingApplication/src/main/java/LibraryPanel.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+public class LibraryPanel extends JPanel {
+    LibraryPanel(){
+        this.add(new JLabel("서재 패널"));
+    }
+}

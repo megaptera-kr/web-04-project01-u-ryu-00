d878bc70eefe8d536608360e6a23bffdd6f4c1e4
u-ryu-00 <lwh76104@gmail.com>
2023-03-20 12:55:27 +0900

    home 브랜치로 바꾸고 작업 시작합니다.

--- application/ReadingApplication/src/main/java/HomePanel.java
+++ application/ReadingApplication/src/main/java/HomePanel.java
@@ -1,7 +1,8 @@
 import javax.swing.*;
 
 public class HomePanel extends JPanel {
-    HomePanel(){
+    HomePanel() {
         this.add(new JLabel("홈 패널"));
+        this.add(new JLabel("홈 패널 작업 시작"));
     }
 }
